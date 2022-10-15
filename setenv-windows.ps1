b75befbe35b11e327f07da2a86d1b1d106051363
_7696122 <7696122@gmail.com>
2022-10-16 07:38:31 +0900

    Update setenv-windows.ps1

--- setenv-windows.ps1
+++ setenv-windows.ps1
@@ -1,26 +1,30 @@
-Function Install-Scoop {
+function Install-Scoop {
     Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
     irm get.scoop.sh | iex
 }
 
 # https://dev.to/scarktt/installing-doom-emacs-on-windows-23ja
-Function Dependent-Doom {
+function Dependent-Doom {
     scoop install git
     scoop install ripgrep
     scoop install fd
 }
 
-Function Install-Doom-Emacs {
+function Install-Doom-Emacs {
     git clone https://github.com/hlissner/doom-emacs .emacs.d
     ~/.emacs.d/bin/doom install
 }
 
-Function Install-Emacs {
+function Install-Emacs {
     scoop bucket add extras
     scoop install emacs
 }
 
-Function Set-Home {
+function Link {
+    mklink /j "C:\Users\USER\.emacs.d" "C:\Users\USER\AppData\Roaming\.emacs.d"
+}
+
+function Set-Home {
     #[System.Environment]::SetEnvironmentVariable('KEY','VALUE')
     [Environment]::SetEnvironmentVariable("HOME", $env:USERPROFILE, "User")
     $env:HOME
